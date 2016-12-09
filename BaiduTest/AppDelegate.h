381533c8b289ae913133f250896f67ad8bbe557c
shendan <shendan@shendandemac-mini.local>
2016-12-09 13:38:00 +0800

    Initial Commit

---
+++ BaiduTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiduTest
+//
+//  Created by shendan on 16/12/9.
+//  Copyright © 2016年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
